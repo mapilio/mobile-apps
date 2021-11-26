5e168b8da9ab3f7991608fdd25928f32d69bfd5d
Muammer Top <muammer@visiosoft.com.tr>
2021-11-26 15:20:31 +0300

    #2 APP / Capture Walkthougher

--- screens/UserProfile.js
+++ screens/UserProfile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ScrollView, View } from "react-native";
-import {UserFeed, UserInfos, Walkthougher} from "../components";
+import {UserFeed, UserInfos} from "../components";
 import { globalStyles } from "../styles/globalStyles";
 
 const UserProfile = ({ navigation }) => {
@@ -12,7 +12,6 @@ const UserProfile = ({ navigation }) => {
           <UserFeed key={item} navigation={navigation} />
         ))}
       </View>
-      <Walkthougher mode={"manual"} />
     </ScrollView>
   );
 };
