eb68862ad6948d2b6ec00992495f7770915d223a
Muammer Top <muammer@visiosoft.com.tr>
2021-12-02 16:59:00 +0300

    reducer hotfix

--- store/store.js
+++ store/store.js
@@ -2,13 +2,21 @@ import {applyMiddleware, createStore} from "redux";
 import reducer from "./combineReducer";
 import thunk from "redux-thunk";
 import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
-import {persistReducer, persistStore} from "redux-persist";
+import {createTransform, persistReducer, persistStore} from "redux-persist";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import JSOG from "jsog";
+
+export const JSOGTransform = createTransform(
+  (inboundState, key) => JSOG.encode(inboundState),
+  (outboundState, key) => JSOG.decode(outboundState)
+);
+
 
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
-  stateReconciler: autoMergeLevel2
+  stateReconciler: autoMergeLevel2,
+  transform: [JSOGTransform]
 };
 
 const persistedReducer = persistReducer(persistConfig, reducer);
