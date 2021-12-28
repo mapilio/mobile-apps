5b85136a8798d7afece1efa0ba542a4866da6625
Muammer Top <muammer@visiosoft.com.tr>
2021-12-28 09:06:44 +0300

    fix to upload and sequence detail

--- components/UserFeed.js
+++ components/UserFeed.js
@@ -5,11 +5,14 @@ import {Routes} from "../navigator/Routes";
 import {userFeedStyles} from "../styles/userProfileStyle";
 import SkeletonPlaceholder from "react-native-skeleton-placeholder";
 import {RFValue} from "react-native-responsive-fontsize";
+import {useDispatch} from "react-redux";
+import {ACTIVE_SEQUENCE, UPDATE_SELECTED_IMAGES} from "../store/actionsName";
 
 const UserFeed = ({navigation, data}) => {
-    const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(false)
+    const dispatch = useDispatch();
     return (
-        !loading ?
+        loading ?
             <SkeletonPlaceholder>
                 <View style={{width: "100%", height: RFValue(95),marginBottom:RFValue(5)}}/>
             </SkeletonPlaceholder>
@@ -17,21 +20,25 @@ const UserFeed = ({navigation, data}) => {
             <TouchableOpacity
                 activeOpacity={0.7}
                 style={userFeedStyles.feedContainer}
-                onPress={() => navigation.navigate(Routes.sequences)}
+                onPress={() => {
+                  dispatch({type: ACTIVE_SEQUENCE, payload: data.sequence_uuid});
+                  dispatch({type: UPDATE_SELECTED_IMAGES, payload: []});
+                  navigation.navigate(Routes.sequences, {id: data.sequence_uuid})
+                }}
             >
                 <View style={userFeedStyles.viewStyle}>
                     <CustomTextBold style={userFeedStyles.dateStyle}>
-                        Aug 14, 2020 - 15:00
+                      {data.exif && JSON.parse(data.exif).DateTime}
                     </CustomTextBold>
-                    <CustomText style={userFeedStyles.descriptionStyle}>
-                        {data.text}
-                    </CustomText>
+                  <CustomText style={userFeedStyles.descriptionStyle}>
+                    {data.count} images
+                  </CustomText>
                 </View>
                 <View>
-                    <Image
-                        source={require("../assets/images/car.png")}
-                        style={userFeedStyles.imageStyle}
-                    />
+                  <Image
+                    style={userFeedStyles.imageStyle}
+                    source={{uri: `${data.path}`}}
+                  />
                 </View>
             </TouchableOpacity>
     );
