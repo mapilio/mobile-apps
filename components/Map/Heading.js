1185cb6ddb88edc5e4aad15df0218d9f02b0500c
muammertop <muammer@visiosoft.com.tr>
2022-11-03 11:00:15 +0300

    fix crash

--- components/Map/Heading.js
+++ components/Map/Heading.js
@@ -3,24 +3,29 @@ import {headingPointGeoJson} from "../../helper/helper";
 import MapboxGL from "@rnmapbox/maps";
 
 const Heading = ({heading, coordinates, markerPath}) => {
+
+  // TODO this icon image crashes on ios
   return (
+    <></>
+/*
     <MapboxGL.ShapeSource
       id={"headingShape"}
       shape={headingPointGeoJson(heading, coordinates)}
     >
-      // TODO this icon image crashes on ios
-      {/*<MapboxGL.SymbolLayer
+
+      <MapboxGL.SymbolLayer
         id='headingSymbol'
         style={{
-          iconImage: markerPath,
+          // iconImage: markerPath,
           iconSize: 1,
           iconAllowOverlap: true,
           iconRotate: ["get", "rotate"],
           iconRotationAlignment: 'map',
         }}
         layerIndex={90}
-        />*/}
+        />
     </MapboxGL.ShapeSource>
+*/
   )
 };
 
