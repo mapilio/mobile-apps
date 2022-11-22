927c1934c07fac4f1a8a91640ef7d9d38e32bbd1
muammertop <muammer@visiosoft.com.tr>
2022-11-22 16:10:22 +0300

    order by position

--- screens/Marketplace.js
+++ screens/Marketplace.js
@@ -11,6 +11,7 @@ import Config from "react-native-config";
 import {useSafeAreaInsets} from "react-native-safe-area-context";
 import {CustomText} from "../highordercomponents";
 import {Document} from "../assets/svg/illustrations";
+import Geolocation from "react-native-geolocation-service";
 
 const Marketplace = ({ navigation }) => {
   const dispatch = useDispatch();
@@ -18,14 +19,30 @@ const Marketplace = ({ navigation }) => {
   const [slidePanel, setSlidePanel] = useState();
   const [toggleSlidePanel, setToggleSlidePanel] = useState(true);
   const {top, bottom} = useSafeAreaInsets();
+  const [currentCoordinate, setCurrentCoordinate] = useState({latitude: 0, longitude: 0});
 
   useEffect(() => {
-    fetchHandler({url: `${Config.SERVICE_URL}/api/get-marketplaces`}).then(({data: {geojson}}) => {
+    Geolocation.getCurrentPosition(({coords: {latitude, longitude}}) => {
+      setCurrentCoordinate({latitude: latitude, longitude: longitude})
+    })
+  }, []);
+
+  useEffect(() => {
+    let url = `${Config.SERVICE_URL}/api/get-marketplaces`
+    const {latitude, longitude} = currentCoordinate;
+
+    if (latitude !== 0 && longitude !== 0) {
+      url += `?lat=${latitude}&lon=${longitude}`
+    }
+
+    fetchHandler({url: url}).then(({data: {geojson}}) => {
       dispatch({type: MARKETPLACE_DATA, payload: JSON.parse(geojson)});
     }).catch(({response: {data: {message}}}) => {
       toastMessage.error(message)
     });
-  }, []);
+
+  }, [currentCoordinate]);
+
 
   return (
     <View style={{flex: 1}}>
