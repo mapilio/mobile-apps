fbb0b954215b38ba017396bf9dfff7832fdedb99
dogukan <dogukan@visiosoft.com.tr>
2023-03-29 13:24:59 +0300

    fix-splash-screen

--- App.js
+++ App.js
@@ -28,6 +28,7 @@ Sentry.init({
 
 OneSignal.setAppId(Config.ONESIGNAL_APP_ID);
 OneSignal.promptForPushNotificationsWithUserResponse();
+SplashScreen.preventAutoHideAsync();
 
 function App() {
   const [fontsLoaded] = useFonts({
@@ -49,10 +50,6 @@ function App() {
     db.startDB();
     db.addColumnIfNotExist('group_id');
     db.addColumnIfNotExist('address');
-
-    (async () => {
-      await SplashScreen.preventAutoHideAsync();
-    })()
   }, []);
 
   useEffect(() => {
