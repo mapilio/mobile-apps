b86983ded20cac413d066a86f0913ed0ad6cb1fa
Dogukan <mail@dogukany.com>
2023-05-24 14:49:10 +0300

    change map keys for android and ios

--- highordercomponents/MapView.js
+++ highordercomponents/MapView.js
@@ -1,9 +1,11 @@
-import React, { Fragment, memo, useRef } from "react";
+import React, { Fragment, memo } from "react";
 import MapLibreGL, { Logger } from "@maplibre/maplibre-react-native";
 import { AttributionButton } from "../components/Map";
 import { appMapStyle } from "../styles/appMapStyle";
-import { View } from "react-native";
+import { Platform, View } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
+import { useActionSheet } from "@expo/react-native-action-sheet";
+import * as Linking from "expo-linking";
 
 MapLibreGL.setAccessToken(null);
 Logger.setLogLevel("error");
@@ -17,14 +19,34 @@ const MapView = ({
   onDidFinishLoadingMap,
   ...props
 }) => {
-  const localMapRef = useRef(null);
+  const { showActionSheetWithOptions } = useActionSheet();
+  
+  const styleKey = Platform.OS === "ios" ? "***REMOVED***" : "yd65qARZZ9UC92nUNfyK";
+  const styleURL ="https://api.maptiler.com/maps/basic-v2-light/style.json?key="
+
+  const showAttributions = () => {
+    showActionSheetWithOptions(
+      {
+        title: `MapLibre Maps SDK for ${Platform.OS.toUpperCase()}`,
+        options: ["Cancel", "© MapTiler", "© OpenStreetMap Contributors"],
+        cancelButtonIndex: 0,
+      },
+      (buttonIndex) => {
+        if (buttonIndex === 1) {
+          Linking.openURL("https://www.maptiler.com/copyright");
+        } else if (buttonIndex === 2) {
+          Linking.openURL("https://www.openstreetmap.org/copyright");
+        }
+      }
+    );
+  };
 
   return (
     <Fragment>
       <MapLibreGL.MapView
         style={mapStyle}
-        styleURL={"https://api.maptiler.com/maps/basic-v2-light/style.json?key=***REMOVED***"}
-        ref={mapRef ? mapRef : localMapRef}
+        styleURL={styleURL + styleKey}
+        ref={mapRef}
         onRegionDidChange={regionChange}
         logoEnabled={false}
         attributionPosition={{right: RFValue(30),bottom: RFValue(10)}}
@@ -43,10 +65,7 @@ const MapView = ({
       </MapLibreGL.MapView>
       <View style={{ ...appMapStyle.mapButtons, width: RFValue(35) }}>
         <AttributionButton
-          showAttribution={() => {
-            localMapRef.current && localMapRef.current?.showAttribution();
-            mapRef && mapRef.current?.showAttribution();
-          }}
+          showAttribution={showAttributions}
         />
       </View>
     </Fragment>
