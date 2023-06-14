2c5a1cadf13f59c99e1c1545e32583982358bb51
Dogukan <mail@dogukany.com>
2023-06-14 12:48:52 +0300

    fix auth pages stylees

--- styles/globalStyles.js
+++ styles/globalStyles.js
@@ -4,9 +4,7 @@ import { RFValue } from "react-native-responsive-fontsize";
 export const globalStyles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingVertical: RFValue(35),
     paddingHorizontal: RFValue(16),
-    paddingBottom: RFValue(100),
     zIndex: 1,
   },
   screenTitle: {
