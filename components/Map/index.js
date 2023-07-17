ad2612004a6e0f1960ac91883fc377a0751834e6
Dogukan <mail@dogukany.com>
2023-07-17 10:42:53 +0300

    maploading and sequencedetail screen export fix

--- components/Map/index.js
+++ components/Map/index.js
@@ -3,3 +3,4 @@ export{default as CenterToUserButton} from "./CenterToUserButton";
 export{default as ProfileButton} from "./ProfileButton";
 export {default as Pano} from './Pano';
 export {default as AttributionButton} from './AttributonButton';
+export {default as MapLoading} from './MapLoading';
--- components/SequenceDetail/index.js
+++ components/SequenceDetail/index.js
@@ -1,1 +1,1 @@
-export default from "./SequenceDetail";
+export {default} from "./SequenceDetail";
