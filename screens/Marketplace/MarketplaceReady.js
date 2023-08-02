2b860750770d91563775d9507f827b2a99775961
Dogukan <mail@dogukany.com>
2023-08-02 14:26:17 +0300

    marketplace ready improvements

--- screens/Marketplace/MarketplaceReady.js
+++ screens/Marketplace/MarketplaceReady.js
@@ -1,35 +1,33 @@
 import React from "react";
-import { View, Image } from "react-native";
-import { CustomText, CustomTextMedium } from "../../highordercomponents";
+import { View, TouchableOpacity } from "react-native";
+import { CustomText, CustomTextMedium,CustomTextBold } from "../../highordercomponents";
 import { marketplaceReceivedStyles } from "../../styles/marketplaceStyles";
 import { Routes } from "../../navigator/Routes";
-import { RFValue } from "react-native-responsive-fontsize";
 import { UPDATE_SELECTED_PROJECT } from "../../store/actionsName";
 import { useDispatch } from "react-redux";
 import {Trans, useTranslation} from "react-i18next";
+import { FocusAwareStatusBar } from "../../components";
+import { ReadyIcon } from "../../assets/svg/illustrations";
 
 const MarketplaceReady = ({ navigation, route }) => {
   const {t} = useTranslation("marketplace")
   const dispatch = useDispatch();
 
   const goToMarketPlace = () => (
-    <CustomText style={marketplaceReceivedStyles.link} onPress={() => navigation.navigate(Routes.marketplace)}/>
+    <CustomTextBold style={marketplaceReceivedStyles.link} onPress={() => navigation.navigate(Routes.marketplace)}/>
   )
 
   return (
     <View style={marketplaceReceivedStyles.container}>
-      <Image
-        source={require("../../assets/images/ready.png")}
-        resizeMode={"contain"}
-        style={marketplaceReceivedStyles.image}
-      />
+      <FocusAwareStatusBar barStyle={"dark-content"} backgroundColor={"#fff"} />
+      <ReadyIcon />
       <CustomTextMedium style={marketplaceReceivedStyles.title}>
         {t("mission_ready")}
       </CustomTextMedium>
       <CustomText style={marketplaceReceivedStyles.description}>
         {t("mission_detail")}
       </CustomText>
-      <CustomText
+      <TouchableOpacity
         style={marketplaceReceivedStyles.button}
         onPress={() => {
           dispatch({
@@ -45,14 +43,16 @@ const MarketplaceReady = ({ navigation, route }) => {
           navigation.navigate(Routes.cameraTab);
         }}
       >
-        {t("start_capture")}
-      </CustomText>
-      <CustomText style={marketplaceReceivedStyles.or}>
+        <CustomText style={marketplaceReceivedStyles.startCapture}>
+          {t("start_capture")}
+        </CustomText>
+      </TouchableOpacity>
+      <CustomTextBold style={marketplaceReceivedStyles.or}>
         {t("or")}
-      </CustomText>
-      <CustomText style={{marginBottom: RFValue(30)}}>
+      </CustomTextBold>
+      <CustomTextMedium style={marketplaceReceivedStyles.backToMarket}>
         <Trans t={t} i18nKey={"back_to_the"} components={[goToMarketPlace()]}/>
-      </CustomText>
+      </CustomTextMedium>
     </View>
   );
 };
