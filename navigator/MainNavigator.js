1c27af84ad06b3459d37ff365d81c0eaa4409b6a
Dogukan <mail@dogukany.com>
2023-08-10 11:30:12 +0300

    migrate config and maintenance mode checks to the mainnavigator

--- navigator/MainNavigator.js
+++ navigator/MainNavigator.js
@@ -1,34 +1,98 @@
-import React, {useEffect} from "react";
-import {createStackNavigator, TransitionPresets} from "@react-navigation/stack";
+import React, { useEffect } from "react";
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
 import TabNavigator from "./TabNavigator";
 import NetInfo from "@react-native-community/netinfo";
-import {UPDATE_CONNECTION_STATUS} from "../store/actionsName";
-import {useDispatch} from "react-redux";
-import {Routes} from "./Routes";
+import { UPDATE_CONNECTION_STATUS } from "../store/actionsName";
+import { useDispatch, useSelector } from "react-redux";
+import { Routes } from "./Routes";
 import StackNavigator from "./StackNavigator";
+import { SET_CONFIG, SET_MAINTENANCE_MODE } from "../store/actionsName";
+import { api, cdn } from "../util/helpers/api";
+import Config from "react-native-config";
 
 const Stack = createStackNavigator();
 
 const MainNavigator = () => {
   const dispatch = useDispatch();
+  const { connection } = useSelector((state) => state.generalReducer);
 
   useEffect(() => {
     const removeListener = NetInfo.addEventListener((state) => {
       dispatch({
         type: UPDATE_CONNECTION_STATUS,
-        payload: {connectionStatus: state.isConnected, connectionType: state.type}
+        payload: {
+          connectionStatus: state.isConnected,
+          connectionType: state.type,
+        },
       });
     });
 
     return () => removeListener();
   }, []);
 
+  useEffect(() => {
+    getConfig();
+    checkMaintenance();
+  }, []);
+
+  const checkMaintenance = async () => {
+    if (connection.connectionStatus) {
+      cdn
+        .get("/v1/hearbeat-check")
+        .then((res) => {
+          if (res.mode) {
+            dispatch({ type: SET_MAINTENANCE_MODE, payload: true });
+          } else {
+            dispatch({ type: SET_MAINTENANCE_MODE, payload: false });
+          }
+        })
+        .catch(() => {
+          dispatch({ type: SET_MAINTENANCE_MODE, payload: true });
+        });
+    }
+  };
+
+  const getConfig = () => {
+    api
+      .get("/config/general?token=" + Config.APP_CONFIG_TOKEN)
+      .then((res) => {
+        dispatch({
+          type: SET_CONFIG,
+          payload: {
+            isMarketOpen: res.config.isMarketOpen,
+            isChallengeOpen: res.config.isChallangeOpen,
+            challengeDescEN: res.config.challengeDescEN,
+            challengeDescTR: res.config.challengeDescTR,
+            challengeURL: res.config.challengeURL,
+            challengeDates: res.config.challengeDates.split("-"),
+          },
+        });
+      })
+      .catch(() => {
+        dispatch({
+          type: SET_CONFIG,
+          payload: {
+            isMarketOpen: false,
+            isChallengeOpen: false,
+          },
+        });
+      });
+  };
+
   return (
-    <Stack.Navigator screenOptions={{headerShown: false, ...TransitionPresets.SlideFromRightIOS}} >
-      <Stack.Screen name={Routes.tabNavigator} component={TabNavigator}/>
-      <Stack.Screen name={Routes.stackNavigator} component={StackNavigator}/>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        ...TransitionPresets.SlideFromRightIOS,
+      }}
+    >
+      <Stack.Screen name={Routes.tabNavigator} component={TabNavigator} />
+      <Stack.Screen name={Routes.stackNavigator} component={StackNavigator} />
     </Stack.Navigator>
-  )
+  );
 };
 
 export default MainNavigator;
