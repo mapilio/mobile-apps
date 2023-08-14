81d45b7b9f331ec898ad579676589a54ae1c9d2b
Dogukan <mail@dogukany.com>
2023-08-14 17:24:15 +0300

    added languages for tab bar labels

--- components/Leaderboard/Board.js
+++ components/Leaderboard/Board.js
@@ -4,6 +4,7 @@ import LeaderboardTabBar from "./LeaderboardTopBar";
 import { LogBox } from "react-native";
 import { useSelector } from "react-redux";
 import SkeletonLoading from "./SkeletonLoading";
+import { useTranslation } from "react-i18next";
 
 const Tab = createMaterialTopTabNavigator();
 LogBox.ignoreLogs(["Sending"]);
@@ -12,29 +13,39 @@ const Board = () => {
   const { users, usersWeek, usersMonth } = useSelector(
     (state) => state.leaderboardReducer
   );
+  const { t } = useTranslation("leaderboard");
 
   return (
     <Tab.Navigator
       tabBarPosition="top"
-      initialRouteName="All Time"
+      initialRouteName="all_time"
       tabBar={(props) => <LeaderboardTabBar {...props} />}
     >
       <Tab.Screen
-        name="All Time"
+        name="all_time"
         component={!users ? SkeletonLoading : Users}
         initialParams={{ type: "all" }}
+        options={{
+          tabBarLabel: t("all_time"),
+        }}
       />
       <Tab.Screen
-        name="This Month"
+        name="last_month"
         component={!usersMonth ? SkeletonLoading : Users}
         initialParams={{ type: "month" }}
+        options={{
+          tabBarLabel: t("last_month"),
+        }}
       />
 
       {usersWeek && usersWeek.length > 6 && (
         <Tab.Screen
-          name="This Week"
+          name="last_week"
           component={!usersWeek ? SkeletonLoading : Users}
           initialParams={{ type: "week" }}
+          options={{
+            tabBarLabel: t("last_week"),
+          }}
         />
       )}
     </Tab.Navigator>
