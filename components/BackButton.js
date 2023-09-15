420f1252733c5be3af4d11006934f1db24c6f34a
Dogukan <mail@dogukany.com>
2023-09-15 12:35:10 +0300

    navigation bar back button size fix

--- components/BackButton.js
+++ components/BackButton.js
@@ -5,6 +5,7 @@ import React from "react";
 import {TouchableOpacity} from "react-native";
 import {useTranslation} from "react-i18next";
 import {useNavigation} from "@react-navigation/native";
+import { RFValue } from "react-native-responsive-fontsize";
 
 const BackButton = ({title}) => {
   const {t} = useTranslation("navigation")
@@ -16,7 +17,7 @@ const BackButton = ({title}) => {
       style={sequenceLeft.container}
       onPress={() => navigation.goBack()}
     >
-      <ArrowLeft/>
+      <ArrowLeft width={RFValue(7)} height={RFValue(12)} />
       <CustomText style={sequenceLeft.backTitle}>
         {t(title || "back")}
       </CustomText>
