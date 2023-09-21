782935975b30961be367b1e8ce8ec06056b70750
Dogukan <mail@dogukany.com>
2023-09-21 16:01:04 +0300

    withcredentials fix

--- util/helpers/api/Cdn.js
+++ util/helpers/api/Cdn.js
@@ -14,9 +14,9 @@ const cdnInstance = axios.create({
 
 cdnInstance.interceptors.request.use(
   async (config) => {
+    config.withCredentials = false;
     config.headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
     config.headers.Expires = 0;
-
     const auth = store.getState().getTokenReducer.auth;
     if (auth?.access_token) {
       config.headers.Authorization = `Bearer ${auth.access_token || auth.token}`;
