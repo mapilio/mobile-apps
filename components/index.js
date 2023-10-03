b8f78b6c4ffb142e598a87ab0540656495aa47be
Dogukan <mail@dogukany.com>
2023-10-03 16:51:34 +0300

    remove unused export

--- components/index.js
+++ components/index.js
@@ -30,6 +30,5 @@ export { default as ToastMessage } from "./Toast/ToastMessage";
 export { default as Loading} from "./Loading";
 export { default as AlertModal} from "./AlertModal";
 export { default as LeaderboardTopBar} from "./Leaderboard/LeaderboardTopBar";
-export { default as NewsletterModal} from "./NewsletterModal";
 export { default as AnimatedCircle} from "./AnimatedCircle";
 
