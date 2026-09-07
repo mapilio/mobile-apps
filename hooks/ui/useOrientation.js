942fab5b0ed46f16dc9b6ba334144e326b230685
ozcandurak <info@ozcandurak.com>
2026-09-07 19:03:20 +0300

    Fix camera orientation and restore GPS after backgrounding

--- hooks/ui/useOrientation.js
+++ hooks/ui/useOrientation.js
@@ -1,62 +1,14 @@
-import { useEffect, useState } from 'react';
-import { Dimensions } from 'react-native';
-import * as ScreenOrientation from 'expo-screen-orientation';
+import { useWindowDimensions } from 'react-native';
 
 /**
- * @param {number} timeout - Timeout in milliseconds. If you want to wait for the orientation to change before updating the state, pass a number here.
  * @returns {string} "PORTRAIT" or "LANDSCAPE"
  * @example
  * const orientation = useOrientation();
- * const orientation = useOrientation(1000);
  */
-const useOrientation = (timeout) => {
-  const initialOrientation =
-    Dimensions.get('window').width > Dimensions.get('window').height ? 'LANDSCAPE' : 'PORTRAIT';
-  const [orientation, setOrientation] = useState(initialOrientation);
+const useOrientation = () => {
+  const { width, height } = useWindowDimensions();
 
-  const initialOrientationHandler = (orientation) => {
-    if (
-      orientation === ScreenOrientation.Orientation.LANDSCAPE_LEFT ||
-      orientation === ScreenOrientation.Orientation.LANDSCAPE_RIGHT ||
-      orientation === ScreenOrientation.Orientation.UNKNOWN
-    ) {
-      setOrientation('LANDSCAPE');
-    } else {
-      setOrientation('PORTRAIT');
-    }
-  };
-
-  const changeOrientation = ({ orientationInfo }) => {
-    const { orientation } = orientationInfo;
-    if (
-      orientation === ScreenOrientation.Orientation.LANDSCAPE_LEFT ||
-      orientation === ScreenOrientation.Orientation.LANDSCAPE_RIGHT
-    ) {
-      setOrientation('LANDSCAPE');
-    } else {
-      setOrientation('PORTRAIT');
-    }
-  };
-
-  useEffect(() => {
-    const listener = ScreenOrientation.addOrientationChangeListener(changeOrientation);
-
-    let timer;
-    if (timeout) {
-      timer = setTimeout(() => {
-        ScreenOrientation.getOrientationAsync().then(initialOrientationHandler);
-      }, timeout);
-    } else {
-      ScreenOrientation.getOrientationAsync().then(initialOrientationHandler);
-    }
-
-    return () => {
-      if (timer) clearTimeout(timer);
-      ScreenOrientation.removeOrientationChangeListener(listener);
-    };
-  }, []);
-
-  return orientation;
+  return width > height ? 'LANDSCAPE' : 'PORTRAIT';
 };
 
 export default useOrientation;
